b18bc21e4ac46e179e1d79f3f75b09b272dbea81
Dmitry Yaskovich <kadavr95@outlook.com>
2022-05-15 00:16:11 +0300

    Add homework solution

--- string_sum.go
+++ string_sum.go
@@ -2,9 +2,10 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
@@ -12,16 +13,80 @@ var (
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	sum := 0
+	totalValues := 0
+	currentValue := 0
+	digits := 0
+	positive := true
+	incorrectOperand := ""
+
+	for _, s := range input {
+		// Incorrect operand checks (fast exit)
+		if incorrectOperand != "" {
+			// Add new symbols to the operand value until complete value will be parsed
+			if s == '-' || s == '+' || s == ' ' {
+				_, conversionError := strconv.Atoi(incorrectOperand)
+				return "", fmt.Errorf("an error occurred: %w", conversionError)
+			} else {
+				incorrectOperand += string(s)
+			}
+		}
+		// Check for wrong operands number (fast exit)
+		if totalValues > 2 {
+			return "", fmt.Errorf("an error occurred: %w", errorNotTwoOperands)
+		}
+		if s == ' ' {
+			continue
+		}
+		if s == '-' || s == '+' {
+			if digits > 0 {
+				// Summation with next value
+				if positive {
+					sum += currentValue
+				} else {
+					sum -= currentValue
+				}
+				totalValues++
+				currentValue = 0
+				digits = 0
+				positive = true
+			}
+			positive = s == '+'
+			continue
+		}
+		// Read new digit
+		n, conversionError := strconv.Atoi(string(s))
+		if conversionError == nil {
+			currentValue = currentValue*10 + n
+			digits++
+		} else {
+			// Manage wrong symbols in the operand
+			incorrectOperand = strconv.Itoa(currentValue) + string(s)
+		}
+
+	}
+	// Finishing operations when string ends
+	if digits > 0 {
+		if positive {
+			sum += currentValue
+		} else {
+			sum -= currentValue
+		}
+		totalValues++
+	}
+	// Checks for the incorrect operand at the end
+	if incorrectOperand != "" {
+		_, conversionError := strconv.Atoi(incorrectOperand)
+		return "", fmt.Errorf("an error occurred: %w", conversionError)
+	}
+	// Check for wrong operands number
+	if totalValues == 0 {
+		return "", fmt.Errorf("an error occurred: %w", errorEmptyInput)
+	}
+	if totalValues != 2 {
+		return "", fmt.Errorf("an error occurred: %w", errorNotTwoOperands)
+	}
+
+	return strconv.Itoa(sum), nil
 }
